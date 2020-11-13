27cc6fb0150c05d0887bd441631088fae4e4676b
mfcust <43455391+mfcust@users.noreply.github.com>
2020-11-13 08:41:56 -0800

    Create function_challenges.py

---
+++ function_challenges.py
@@ -0,0 +1,52 @@
+# 1) Write a Python function to find the max of three numbers. HINT: Your function will need three parameters, and you will need to use logical operators.
+
+
+
+
+
+
+
+
+
+
+# 2) Create a python function to sum all of the numbers in the following list:
+list1 = [99, 50,22,43.1,3,20,1000,0.3,496]
+#HINT: The parameter your function will take is the list! A for loop will also help you accomplish this task.
+
+
+
+
+
+
+
+
+
+# 3) Create a function that reverses the following string:
+sent = '!!!gnirts eht gnisrever boj eciN'
+#HINT: You will need to populate an empty string variable starting with the last character in your string. A while loop can help you do this, as well as the len()
+#function.
+#SUPER HINT: The length of a string is exactly 1 greater than the
+
+
+
+
+
+
+
+
+
+
+# 4) Create a function that tells you how many upper case characters and how many lower case characters are in the given string below:
+#HINT: There are two interesting functions we haven't talked about called is.upper() and is.lower(). They may come in handy for your boolean expressions.
+
+string1 = 'I am A sTrinG thAT haS BotH lowERCase aNd uPPercAse LeTTerS.'
+
+
+
+
+
+
+
+
+
+
